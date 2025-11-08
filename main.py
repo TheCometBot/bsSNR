336f912c2f0e517443977d97b859d572b384c5aa
Tarani-2020 <nathan.langen@web.de>
2025-11-08 20:39:57 +0100

    yt

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
 import threading
+import discord
 from discord.ext import commands
 from discord import Intents
-from flask import Flask, redirect, url_for
-
+from flask import Flask, redirect, url_for, request
+import requests
+import xml.etree.ElementTree as ET
+import asyncio
 
 # -------- Discord Setup --------
 intents = Intents.default()
@@ -52,6 +55,19 @@ async def reload(ctx):
         msg += f"\n❌ Fehler: {', '.join(failed)}"
     await ctx.respond(msg, ephemeral=True)
 
+async def send_new_yt_video(title, author, link, thumbnail_url):
+    channel = bot.get_channel(1436649248610582528)
+    if channel:
+        embed = discord.Embed(
+            title=f"{author}: Neues Video!",
+            url=link,
+            description=f"{title}\n@everyone",
+            color=discord.Color.red(),
+        )
+        embed.set_thumbnail(url=thumbnail_url)
+        await channel.send(embed=embed)
+
+
 # -------- Flask Setup --------
 app = Flask(__name__)
 
@@ -73,9 +89,57 @@ def privacy():
     with open("PRIVACY.html", 'r', encoding='utf-8') as f:
         return f.read()
     
+@app.route("/websub/callback", methods=["GET", "POST"])
+def websub_callback():
+    if request.method == "GET":
+        challenge = request.args.get("hub.challenge")
+        if challenge:
+            return challenge, 200
+        return "Missing challenge", 400
+    print("Youtube-Update")
+    data = request.data.decode("utf-8")
+
+    root = ET.fromstring(data)
+
+    entry = root.find("{http://www.w3.org/2005/Atom}entry")
+    if entry is not None:
+        ns = {
+            "yt": "http://www.youtube.com/xml/schemas/2015",
+            "atom": "http://www.w3.org/2005/Atom"
+        }
+
+        video_id = entry.find("yt:videoId", ns).text
+        channel_id = entry.find("yt:channelId", ns).text
+        title = entry.find("atom:title", ns).text
+        author = entry.find("atom:author/atom:name", ns).text
+        link = entry.find("atom:link", ns).attrib.get("href")
+        thumbnail = f"https://img.youtube.com/vi/{video_id}/hqdefault.jpg"
+        asyncio.create_task(send_new_yt_video(title, author, link, thumbnail))
+
+
+def subscribe():
+    callback_url = "https://creeper-7rup.onrender.com/websub/callback/"
+    print("Callback:", callback_url)
+    hub_url = "https://pubsubhubbub.appspot.com/subscribe"
+    channel_ids = ["UCxzx7sdLPG9XzxC-supGbiw", "UCg7aqczRrjRMTvyQD9FF0Yg"]
+    topics = []
+    for cid in channel_ids:
+        topics.append(f"https://www.youtube.com/channel/{cid}")
+    for topic in topics:
+        data = {
+            "hub.mode": "subscribe",
+            "hub.topic": topic,
+            "hub.callback": callback_url,
+            "hub.verify": "async"
+        }
+        response = requests.post(hub_url, data=data)
+        print("Subscription sended:", response.status_code, response.text)
+    print("All Subscriptions done.")
+    
 
 # -------- Helper to run bot in a thread --------
 def run_bot():
+    subscribe()
     bot.run(os.getenv("BOT_TOKEN"))
 
 # -------- Main --------
